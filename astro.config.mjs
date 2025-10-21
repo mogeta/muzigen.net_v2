2872e28c083582065e1e14a6f91b58d4800254f4
mogeta <muzigen@gmail.com>
2025-10-21 11:44:05 +0900

    add Google Analytics integration and configure Partytown for improved performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    output: 'static',
+
+    vite: {
+        plugins: [tailwindcss()]
+    },
+
+    integrations: [partytown({
+        config: {
+            // Google AnalyticsのためにdataLayer.pushを転送
+            forward: ["dataLayer.push"],
+        },
+    })]
 });
